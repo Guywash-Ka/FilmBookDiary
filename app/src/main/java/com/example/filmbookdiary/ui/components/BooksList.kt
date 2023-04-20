f811d8fa2f7977d882dfeee8a1bc6c54520bd9fb
Guywash-Ka <rodvlasov2003@yandex.ru>
2023-04-20 23:28:20 +0300

    Changed BookList style

--- app/src/main/java/com/example/filmbookdiary/ui/components/BooksList.kt
+++ app/src/main/java/com/example/filmbookdiary/ui/components/BooksList.kt
@@ -1,6 +1,7 @@
 package com.example.filmbookdiary.ui.components
 
 import android.net.Uri
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
@@ -16,9 +17,19 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.ColorPainter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.graphics.drawable.toIcon
+import coil.compose.rememberAsyncImagePainter
+import coil.compose.rememberImagePainter
+import com.example.filmbookdiary.FilmBookDiaryApplication.Companion.maxRating
+import com.example.filmbookdiary.R
 import com.example.filmbookdiary.data.Book
 import com.example.filmbookdiary.data.Film
 import com.example.filmbookdiary.viewmodel.BookViewModel
@@ -46,65 +57,72 @@ fun BookPage(
         elevation = 10.dp,
         modifier = modifier.clickable { onBookClicked(bookID) }
     ) {
-        Column(modifier = modifier.background(color = colors.primary)) {
-            Box(contentAlignment = Alignment.TopEnd) {
-                GlideImage(
-                    imageModel = { bookImageUri },
-                    modifier = modifier.height(280.dp)
+        Box() {
+            Row(modifier = modifier.background(color = colors.primary)) {
+//                GlideImage(
+//                    imageModel = { bookImageUri },
+//                    modifier = modifier.height(240.dp).width(140.dp)
+//                )
+                Image(
+                    painter = rememberAsyncImagePainter(bookImageUri),
+                    contentDescription = "My content description",
+                    contentScale = ContentScale.FillHeight,
+                    modifier = modifier.height(240.dp).width(140.dp)
                 )
-                IconButton(onClick = {
-                    coroutineScope.launch {
-                        bookViewModel.removeBook(bookViewModel.getBook(bookID))
+                Row(
+                    modifier = modifier.fillMaxWidth(1f),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Column() {
+                        Text(
+                            modifier = modifier.padding(start = 8.dp, end = 30.dp),
+                            fontWeight = FontWeight.ExtraBold,
+                            text = bookName,
+                            style = typography.h4,
+                            color = colors.onPrimary,
+                            maxLines = 1,
+                            overflow = TextOverflow.Ellipsis
+                        )
+                        Text(
+                            modifier = modifier.padding(start = 8.dp),
+                            fontWeight = FontWeight.Bold,
+                            text = bookAuthor,
+                            style = typography.h6,
+                            color = colors.onPrimary,
+                            maxLines = 1,
+                            overflow = TextOverflow.Ellipsis
+                        )
+                        Text(
+                            modifier = modifier.padding(start = 16.dp, bottom = 8.dp),
+                            text = bookDescription,
+                            color = colors.onPrimary,
+                            maxLines = 2,
+                            overflow = TextOverflow.Ellipsis
+                        )
                     }
-                }) {
-                    Icon(
-                        Icons.Filled.Clear,
-                        contentDescription = "Remove element",
-                        tint = colors.onPrimary
-                    )
+                    if (bookRating != null) {
+                        Text(
+                            text = "$bookRating/$maxRating",
+                            fontWeight = FontWeight.ExtraBold,
+                            style = typography.h4,
+                            color = colors.onPrimary
+                        )
+                    }
                 }
             }
-            Row(
-                modifier = modifier.fillMaxWidth(1f),
-                horizontalArrangement = Arrangement.SpaceBetween
-            ) {
-                Column() {
-                    Text(
-                        modifier = modifier.padding(start = 8.dp),
-                        fontWeight = FontWeight.ExtraBold,
-                        text = bookName,
-                        style = typography.h4,
-                        color = colors.onPrimary,
-                        maxLines = 1,
-                        overflow = TextOverflow.Ellipsis
-                    )
-                    Text(
-                        modifier = modifier.padding(start = 8.dp),
-                        fontWeight = FontWeight.Bold,
-                        text = bookAuthor,
-                        style = typography.h6,
-                        color = colors.onPrimary,
-                        maxLines = 1,
-                        overflow = TextOverflow.Ellipsis
-                    )
-                    Text(
-                        modifier = modifier.padding(start = 16.dp, bottom = 8.dp),
-                        text = bookDescription,
-                        color = colors.onPrimary,
-                        maxLines = 2,
-                        overflow = TextOverflow.Ellipsis
-                    )
+            IconButton(onClick = {
+                coroutineScope.launch {
+                    bookViewModel.removeBook(bookViewModel.getBook(bookID))
                 }
-                if (bookRating != null) {
-                    Text(
-                        text = "$bookRating/10",
-                        fontWeight = FontWeight.ExtraBold,
-                        style = typography.h4,
-                        color = colors.onPrimary
-                    )
-                }
+            }) {
+                Icon(
+                    Icons.Filled.Clear,
+                    contentDescription = "Remove element",
+                    tint = colors.onPrimary
+                )
             }
         }
+
     }
 }
 
@@ -115,6 +133,7 @@ fun BookList(
     onBookClicked: (UUID) -> Unit,
     bookViewModel: BookViewModel
 ) {
+
     LazyColumn(verticalArrangement = Arrangement.spacedBy(16.dp), contentPadding = PaddingValues(all = 8.dp)) {
         items(
             items = books,
@@ -131,5 +150,85 @@ fun BookList(
                 )
             },
         )
+    }
+}
+
+@Preview
+@Composable
+fun BookPagePreview(
+    modifier: Modifier = Modifier,
+    bookID: UUID = UUID.randomUUID(),
+    bookImageUri: Uri = Uri.parse("android.resource://com.example.filmbookdiary/" + R.drawable.empty_photo),
+    bookName: String = "Sherlock Holms",
+    bookAuthor: String = "William Shakespeare",
+    bookDescription: String = "Once upon a time lived a family, Once upon a time lived a family, Once upon a time lived a family, Once upon a time lived a family",
+    bookRating: Int? = 2
+) {
+
+    Card(
+        shape = RoundedCornerShape(topEnd = 16.dp, topStart = 16.dp),
+        elevation = 10.dp,
+        modifier = modifier.clickable {  }
+    ) {
+        Box() {
+
+            Row(modifier = modifier.background(color = colors.primary)) {
+                Image(
+                    painter = painterResource(R.drawable.niceguys_photo),
+                    contentDescription = "Book image",
+                    contentScale = ContentScale.FillHeight,
+                    modifier = modifier
+                        .width(140.dp)
+                        .height(240.dp)
+                )
+                Row(
+                    modifier = modifier.fillMaxWidth(1f),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Column() {
+                        Text(
+                            modifier = modifier.padding(start = 8.dp, end = 30.dp),
+                            fontWeight = FontWeight.ExtraBold,
+                            text = bookName,
+                            style = typography.h4,
+                            color = colors.onPrimary,
+                            maxLines = 3,
+                            overflow = TextOverflow.Ellipsis
+                        )
+                        Text(
+                            modifier = modifier.padding(start = 8.dp),
+                            fontWeight = FontWeight.Bold,
+                            text = bookAuthor,
+                            style = typography.h6,
+                            color = colors.onPrimary,
+                            maxLines = 1,
+                            overflow = TextOverflow.Ellipsis
+                        )
+                        Text(
+                            modifier = modifier.padding(start = 16.dp, bottom = 8.dp),
+                            text = bookDescription,
+                            color = colors.onPrimary,
+                            maxLines = 2,
+                            overflow = TextOverflow.Ellipsis
+                        )
+                    }
+                    if (bookRating != null) {
+                        Text(
+                            text = "$bookRating/$maxRating",
+                            fontWeight = FontWeight.ExtraBold,
+                            style = typography.h4,
+                            color = colors.onPrimary
+                        )
+                    }
+                }
+            }
+            IconButton(onClick = {}, modifier.align(Alignment.TopEnd)) {
+                Icon(
+                    Icons.Filled.Clear,
+                    contentDescription = "Remove element",
+                    tint = Color.Black
+                )
+            }
+        }
     }
 }
